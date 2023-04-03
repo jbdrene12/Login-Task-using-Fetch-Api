0a11b258e45208c01b0edfaaa355ed98d9fda771
jbdrene12 <123716640+jbdrene12@users.noreply.github.com>
2023-04-03 13:23:25 +0530

    commit 1:23 PM 3/4/23

--- task-Userlogin/script-crud.js
+++ task-Userlogin/script-crud.js
@@ -1,3 +1,4 @@
+/*  THIS TABLE CONTAINS FUNCTIONS TO PERFORM CRUD OPERATIONS USING FETCH API   */
 
 /* GET */
 
@@ -15,14 +16,16 @@ async function get(access, url) {   //  FETCH - GET
 /* PUT */
 
 async function put(url) {    //  FETCH - PUT REQUEST
-    if (/^(male)$|^(female)$/.test(document.getElementById("gender").value) && /^(active)$|^(inactive)$/.test(document.getElementById("status").value)) {
+    if (/^(male)$|^(female)$/.test(document.getElementById("gender").value.toLowerCase())
+        && /^(active)$|^(inactive)$/.test(document.getElementById("status").value.toLowerCase())
+        && /^[a-z][a-z0-9]+\@[a-z][a-z0-9]+\.[a-z0-9]+$/.test(document.getElementById("email").value)) {
         if (confirm("Do you want to write the changes?")) {
             fetch(url, {
                 method: "PUT",
                 body: JSON.stringify({
                     name: document.getElementById("full-name").value,
                     email: document.getElementById("email").value,
-                    gender: document.getElementById("gender").value.toLowerCase(),
+                    gender: document.getElementById("gender").value,
                     status: document.getElementById("status").value
                 }),
                 headers: {
@@ -34,14 +37,20 @@ async function put(url) {    //  FETCH - PUT REQUEST
                 .then(response => response.json())
                 .then(json => console.log(json));      // DISPLAY OBJECT WITH NEW VALUES
             alert("Success")        // DISPLAY SUCCESS
-            document.getElementById("update-user").innerHTML = '<input type="button" value="Updated Succesfully" class="btn btn-success float-end px-5 mx-4 mb-4" disabled >'
+            document.getElementById("update-user").innerHTML =
+                '<input type="button" value="Updated Succesfully" class="btn btn-success float-end px-5 mx-4 mb-4" disabled >'
         }
     }
-    else if (/^(male)$|^(female)$/.test(document.getElementById("gender").value)) {
-        alert("Check status field")
+    else if (/^(male)$|^(female)$/.test(document.getElementById("gender").value)
+        && /^[a-z][a-z0-9]+\@[a-z][a-z0-9]+\.[a-z0-9]+$/g.test(document.getElementById("gender").value)) {
+        alert("Check status field");
     }
-    else if (/^(active)$|^(inactive)$/.test(document.getElementById("status").value)) {
-        alert("Check gender field")
+    else if ((/^(male)$|^(female)$/.test(document.getElementById("gender").value))
+        && /^(active)$|^(inactive)$/.test(document.getElementById("status").value.toLowerCase())) {
+        alert("Check email field");
+    }
+    else {
+        alert("Check gender field");
     }
 }
 
@@ -66,13 +75,15 @@ async function toDelete(url) {
 /* POST */
 
 async function post(url) {          //  FETCH - POST
-    if (/^(male)$|^(female)$/.test(document.getElementById("gender").value) && /^(active)$|^(inactive)$/.test(document.getElementById("status").value)) {
+    if (/^(male)$|^(female)$/.test(document.getElementById("gender").value.toLowerCase())
+        && /^(active)$|^(inactive)$/.test(document.getElementById("status").value.toLowerCase())
+        && /^[a-z][a-z0-9]+\@[a-z][a-z0-9]+\.[a-z0-9]+$/.test(document.getElementById("email").value)) {
         fetch(url, {
             method: "POST",
             body: JSON.stringify({
                 name: document.getElementById("full-name").value,
                 email: document.getElementById("email").value,
-                gender: document.getElementById("gender").value.toLowerCase(),
+                gender: document.getElementById("gender").value,
                 status: document.getElementById("status").value
             }),
             headers: {
@@ -83,12 +94,18 @@ async function post(url) {          //  FETCH - POST
         ).then(json => console.log(json));           //  DISPLAY ADDED VALUE AS OBJECT
 
         alert("Success")                                //  POPUP MESSAGE - SUCCESS
-        document.getElementById("add-user").innerHTML = '<input type="button" value="Added" class="btn btn-primary float-end px-5 mx-4 mb-4 disabled">'
+        document.getElementById("add-user").innerHTML =
+            '<input type="button" value="Added" class="btn btn-primary float-end px-5 mx-4 mb-4 disabled">';
     }
-    else if (/^(male)$|^(female)$/.test(document.getElementById("gender").value)) {
-        alert("Check status field")
+    else if (/^(male)$|^(female)$/.test(document.getElementById("gender").value)
+        && /^[a-z][a-z0-9]+\@[a-z][a-z0-9]+\.[a-z0-9]+$/g.test(document.getElementById("gender").value)) {
+        alert("Check status field");
+    }
+    else if ((/^(male)$|^(female)$/.test(document.getElementById("gender").value))
+        && /^(active)$|^(inactive)$/.test(document.getElementById("status").value.toLowerCase())) {
+        alert("Check email field");
     }
     else {
-        alert("Check gender field")
+        alert("Check gender field");
     }
 }
--- task-Userlogin/script-sort.js
+++ task-Userlogin/script-sort.js
@@ -1,20 +1,23 @@
-function sortColumn(array, column) {
-    let nameArray = [];
-    let idArray = [];
-    let emailArray = [];
-    let tempSortedArray = [];
-    let idSort = [];
-    let nameSort = [];
-    let emailSort = [];
+    //  SORT AN ARRAY OF DATA (ASCENDING)
+
+function sortColumn(array, column) {    
+    //  'array' - ARRAY OF DATA, 'column' - COLUMN TO SORT ('id' or 'name')
+    let nameArray = []; //  COLLECTION OF ALL NAMES
+    let idArray = [];   //  COLLECTION OF ALL IDS
+    let emailArray = [];//  COLLECTION OF ALL EMAIL-IDS
+    let idSort = [];    //  SORTED ID ARRAY
+    let nameSort = [];  //  SORTED NAME ARRAY
+    let emailSort = []; //  SORTED EMAIL 
+    
     for (i in array) {
         idArray.push(array[i][0]);
         nameArray.push(array[i][1]);
         emailArray.push(array[i][2]);
     }
+
     idArray.sort((x, y) => x - y);
     nameArray.sort();
     emailArray.sort();
-    tempSortedArray.push(idArray, nameArray, emailArray);
     if (column == "id") {
         for (i in idArray) {
             idSort.push(...(array.filter((innerArray) => innerArray[0] == idArray[i])))
@@ -30,9 +33,10 @@ function sortColumn(array, column) {
         console.log(nameSort)
         return nameSort
     }
+    /* BELOW CODE WON'T WORK */
     else {
         for (i in emailArray) {
-            emailSort.push(array.filter((innerArray) => innerArray[2] == i))
+            emailSort.push(array.filter((innerArray) => innerArray))
         }
         return emailSort
     }
